51293538e34f75777ca6c59c8f41d442ba534835
Matt Hicks <matt@outr.com>
2016-10-01 11:48:39 -0500

    Added args support to interpolation.

--- src/main/scala/com/outr/neo4akka/Macros.scala
+++ src/main/scala/com/outr/neo4akka/Macros.scala
@@ -1,6 +1,5 @@
 package com.outr.neo4akka
 
-import org.neo4j.cypher.internal.frontend.v3_0.ast.Statement
 import org.neo4j.cypher.internal.frontend.v3_0.parser.CypherParser
 
 import scala.annotation.compileTimeOnly
@@ -15,16 +14,26 @@ object Macros {
     c.prefix.tree match {
       case Apply(_, List(Apply(_, rawParts))) => {
         val parts = rawParts map { case t @ Literal(Constant(const: String)) => (const, t.pos) }
-        parts match {
-          case List((raw, pos)) => {
-            val parser = new CypherParser
-            parser.parse(raw)
-            c.Expr[CypherQuery](q"""CypherQuery($raw)""")
+
+        val b = new StringBuilder
+        parts.zipWithIndex.foreach {
+          case ((raw, pos), index) => {
+            if (index > 0) {
+              b.append(s"{arg$index}")
+            }
+            b.append(raw)
           }
-          case _ => c.abort(c.enclosingPosition, "Bad usage of cypher interpolation.")
         }
+        val argsMap = args.zipWithIndex.map {
+          case (value, index) => {
+            s"arg${index + 1}" -> value
+          }
+        }.toMap
+        val parser = new CypherParser
+        parser.parse(b.toString())
+        c.Expr[CypherQuery](q"""CypherQuery(${b.toString()}, $argsMap)""")
       }
-      case _ => c.abort(c.enclosingPosition, "Bad usage of cypher interpolation.")
+      case _ => c.abort(c.enclosingPosition, "Bad usage of cypher interpolation 2.")
     }
   }
 }
--- src/test/scala/specs/CypherSpec.scala
+++ src/test/scala/specs/CypherSpec.scala
@@ -7,12 +7,14 @@ import org.scalatest.{Matchers, WordSpec}
 class CypherSpec extends WordSpec with Matchers {
   "Cypher" should {
     "interpolate a valid query" in {
-      val q = cypher"MATCH (p: Person {name: {personName}}) RETURN p"
+      val personName = "Tom Hanks"
+      val q = cypher"MATCH (p: Person {name: $personName}) RETURN p"
       q should not be null
+      q.query should equal("MATCH (p: Person {name: {arg1}}) RETURN p")
+      q.args should be(Map("arg1" -> "Tom Hanks"))
     }
-    "interpolate an invalid query" in {
-      val q = cypher"MATCH BAD (p: Person {name: {personName}}) RETURN p"
-      q should not be null
+    "fail to compile with an invalid query interpolation" in {
+      """cypher"MATCH BAD (p: Person {name: {personName}}) RETURN p"""" shouldNot compile
     }
   }
 }
